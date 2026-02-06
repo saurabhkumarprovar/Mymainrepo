5d4d5519148417b89b53da6f7f08ff34057b5bac
saurabh.kumar <saurabh.kumar@DESKTOP-AM101LS>
2026-02-06 11:13:27 +0530

    First commit to mymainproject

---
+++ GitTryProject/src/pageobjects/MyPageObject.java
@@ -0,0 +1,21 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="MyPageObject"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="Amazon"
+     )             
+public class MyPageObject {
+
+	@TextType()
+	@FindBy(xpath = "//div[@data-cel-widget='Navigation-desktop-navbar']//input[@id='twotabsearchtextbox']")
+	public WebElement searchAmazonIn;
+			
+}
